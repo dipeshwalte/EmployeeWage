d39b0da9c639b881d3d9fe4d449544c88a2554aa
Dipesh Walte <dipesh4edutopia@users.noreply.github.com>
2020-12-11 20:14:18 +0530

    Part time work added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -8,6 +8,11 @@ then
    echo Employee is present
    dailyWage=$(($wagePerHour*$fullDayHour))
    echo Daily Wage is - $dailyWage
+	if [ $((RANDOM%2)) -eq 0 ]
+   then
+      echo 'Employee did part time work'
+      dailyWage=$(($dailyWage+$wagePerHour*8))
+      echo 'Wage with Part time work' $dailyWage
 else
    echo Employee is absent
    echo Daily Wage is - $dailyWage
