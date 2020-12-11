d4526bdfc3a326cf0150a1057c0d4acb7e7173da
Dipesh Walte <dipesh4edutopia@users.noreply.github.com>
2020-12-11 20:24:51 +0530

    Switch Case Added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
-echo Welcome to Employee Wage computation
+echo Welcome to Employee wage computation
 dailyWage=0
 wagePerHour=20
 fullDayHour=8
-if [ $((RANDOM%2)) -eq 0 ]
-then
+empPresentFlag=$((RANDOM%2))
+case $empPresentFlag in
+0)
    echo Employee is present
    dailyWage=$(($wagePerHour*$fullDayHour))
    echo Daily Wage is - $dailyWage
-	if [ $((RANDOM%2)) -eq 0 ]
+   if [ $((RANDOM%2)) -eq 0 ]
    then
-      echo 'Employee did part time work'
-      dailyWage=$(($dailyWage+$wagePerHour*8))
-      echo 'Wage with Part time work' $dailyWage
-else
+         echo 'Employee did part time work'
+         dailyWage=$(($dailyWage+$wagePerHour*8))
+         echo 'Wage with Part time work' $dailyWage
+
+   fi;;
+1)
    echo Employee is absent
    echo Daily Wage is - $dailyWage
-fi
+;;
+*)
+   echo Something went wrong
+;;
+esac
