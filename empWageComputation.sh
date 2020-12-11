cfbb8b280f60284209e769d7305349d031d641a4
Dipesh Walte <dipesh4edutopia@users.noreply.github.com>
2020-12-11 19:54:51 +0530

    Daily wage computation added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 echo Welcome to Employee Wage computation
+dailyWage=0
+wagePerHour=20
+fullDayHour=8
 if [ $((RANDOM%2)) -eq 0 ]
 then
-   echo Employee is Present
+   echo Employee is present
+   dailyWage=$(($wagePerHour*$fullDayHour))
+   echo Daily Wage is - $dailyWage
+   if [ $((RANDOM%2)) -eq 0 ]
+   then
+      echo 'Employee did part time work'
+      dailyWage=$(($dailyWage+$wagePerHour*8))
+      echo 'Wage with Part time work' $dailyWage
+   fi
 else
-	echo Employee is Absent
+   echo Employee is absent
+   echo Daily Wage is - $dailyWage
 fi
