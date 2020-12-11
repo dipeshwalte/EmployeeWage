5dcdcc160090f8ae45c06edeff59c2c99deb64cd
Dipesh Walte <dipesh4edutopia@users.noreply.github.com>
2020-12-11 20:02:51 +0530

    Wrong use case inserted

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -8,12 +8,6 @@ then
    echo Employee is present
    dailyWage=$(($wagePerHour*$fullDayHour))
    echo Daily Wage is - $dailyWage
-   if [ $((RANDOM%2)) -eq 0 ]
-   then
-      echo 'Employee did part time work'
-      dailyWage=$(($dailyWage+$wagePerHour*8))
-      echo 'Wage with Part time work' $dailyWage
-   fi
 else
    echo Employee is absent
    echo Daily Wage is - $dailyWage
