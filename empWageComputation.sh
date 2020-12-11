19af9a78ca1ac121d3814623531b47863a9cad42
Dipesh Walte <dipesh4edutopia@users.noreply.github.com>
2020-12-11 21:33:14 +0530

    refactored with wage function

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -6,32 +6,39 @@ fullDayHour=8
 workingDaysInMonth=0
 wagesForMonth=0
 workingHours=0
+getWorkHours(){
+	if [ $1 -eq 0 ]
+	then
+	dailyWorkHours=16
+	else
+	dailyWorkHours=8
+	fi
+}
 while [ $workingDaysInMonth -lt 20 -a $workingHours -lt 100 ]
 do
    empPresentFlag=$((RANDOM%2))
    case $empPresentFlag in
    0)
+		 dailyWorkHours=0
        echo Employee is present
        dailyWage=$(($wagePerHour*$fullDayHour))
-       echo Daily Wage is - $dailyWage
        wagesForMonth=$(($wagesForMonth+$dailyWage))
-       if [ $((RANDOM%2)) -eq 0 ]
+		 partTimeFlag=$((RANDOM%2))
+		 getWorkHours $partTimeFlag
+       workingHours=$(($workingHours+$dailyWorkHours))
+       if [ $partTimeFlag -eq 0 ]
        then
             echo 'Employee did part time work'
             dailyWage=$(($dailyWage+$wagePerHour*8))
-            echo 'Wage with Part time work' $dailyWage
             wagesForMonth=$(($wagesForMonth+$wagePerHour*8))
-            workingHours=$(($workingHours+8))
             if [ $workingHours -gt 100 ]
             then
                break
             fi
        fi
-      workingHours=$(($workingHours+8))
       workingDaysInMonth=$(($workingDaysInMonth+1));;
     1)
       echo Employee is absent
-      echo Daily Wage is - $dailyWage
     ;;
     *)
        echo Something went wrong
